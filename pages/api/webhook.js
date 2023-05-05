88b51505533dd4fbcb0352b5d0859e3c16981ab5
dineshsai463 <dineshsai463@example.com>
2023-05-05 22:44:21 +0530

    first commit

---
+++ pages/api/webhook.js
@@ -0,0 +1,45 @@
+import {mongooseConnect} from "@/lib/mongoose";
+const stripe = require('stripe')(process.env.STRIPE_SK);
+import {buffer} from 'micro';
+import {Order} from "@/models/Order";
+
+ const endpoint_secret = 'whsec_cdb418084bed8e30547e42c2d2428ec149165fb265cb160fd751252b3f013176'
+
+export default async function handler(req,res) {
+  await mongooseConnect();
+  const sig = req.headers['stripe-signature'];
+
+  let event;
+
+  try {
+    event = stripe.webhooks.constructEvent(await buffer(req), sig, endpoint_secret);
+  } catch (err) {
+    res.status(400).send(`Webhook Error: ${err.message}`);
+    return;
+  }
+
+  // Handle the event
+  switch (event.type) {
+    case 'checkout.session.completed':
+      const data = event.data.object;
+      const orderId = data.metadata.orderId;
+      const paid = data.payment_status === 'paid';
+      if (orderId && paid) {
+        await Order.findByIdAndUpdate(orderId,{
+          paid:true,
+        })
+      }
+      break;
+    default:
+      console.log(`Unhandled event type ${event.type}`);
+  }
+
+  res.status(200).send('ok');
+}
+
+export const config = {
+  api: {bodyParser:false,}
+};
+
+// bright-thrift-cajole-lean
+// acct_1Lj5ADIUXXMmgk2a
